11d8571aab9aa88be9088d83eb4d870d8a60d1e7
Amit Pandey <amitpande184@gmail.com>
2024-09-18 18:25:44 +0530

    collab message

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Buttonbox from './component/Buttonbox';
 
 const App = () => {
+  let [Color , setColor]  =   useState("#374151")
+
+  let handlerColor = (newColor) =>{
+    setColor(newColor)
+  }
+
   return (
-    <div className='h-screen w-full flex justify-center  items-center bg-gray-700'>
-     <div  className='h-[30%] w-[40%] flex flex-col justify-center items-center bg-slate-400 rounded-lg'>
-       <h1 className='text-white text-4xl font-semibold font-sans'>Welcome to React!</h1>
-       <p className='text-2xl text-white'>This is a simple React page</p>
-       <p className='text-2xl text-white'>Thank you</p>
-     </div>
+    <div className='h-screen w-full flex justify-center  items-center' 
+     style={{backgroundColor:Color}}>
+      <Buttonbox  onColorChange={handlerColor}  />
     </div>
   );
 };
